abd609a54a2ce02b3eb58e7778326690b7103b99
saleem191906 <syedsaleem191906@gmail.com>
2025-11-24 18:13:13 +0530

    my day1 practice

---
+++ day 1.py
@@ -0,0 +1,59 @@
+a=4
+a=2
+print(id(a))
+print(a)
+b='hello iam saleem sayyad'
+print(b)
+c= "saleem is nagul meera's son"
+print(c)
+d="""tngo's colony
+near signature pg 
+hyderabad"""
+print(d)
+
+
+x =9
+y = 2
+# print sum, product, floor division (//), remainder (%)
+print(x + y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+
+
+s = "saleem"
+print(type(s))
+s = int(s) #converted"123" to 123
+print(type(s))
+# convert and add
+f = 4.7
+f = int(f)
+print(f)
+# convert f to int and print
+
+
+
+
+
+ 
+f=4.7
+print(f)
+print(int(f))
+print(float(f))
+print(type(f))
+
+
+k=4+1j
+print(k)
+print(type(k))
+print(k.real)
+print(k.imag)
+print(int(k.real))
+print(float(k.imag))
+print(int(k))
+print(complex(2,3))
+
+
+print("10" == 10)          # False
+print(5 < 10 <= 15)        # True
